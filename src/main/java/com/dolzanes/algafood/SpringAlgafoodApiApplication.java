75049ef2cbc533e84eeff1695b737232c9608669
FabianoDolzanes <fdolzanes1@gmail.com>
2022-12-27 19:12:27 -0400

    first commit

---
+++ src/main/java/com/dolzanes/algafood/SpringAlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.dolzanes.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlgafoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dolzanes/algafood/SpringAlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolzanes.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
